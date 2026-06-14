730d5fb67987d0bec6365c871558b9af40f592f5
ndemanou237 <ndemanoudonald9@gmail.com>
2026-06-14 19:26:47 +0100

    second hier

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 console.log("hello, node !")
 
 const express = require('express')
+const favicon = require('serve-favicon')
 const morgan = require('morgan')
-const { success } = require('./helper')
+const { success, getUniqueId } = require('./helper')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 
-app.use(morgan('dev'))
+app
+    // .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
 
 app.get('/', (req,res) => res.send('hello again, express  !'))
 app.get('/favicon.ico', (req,res) => res.sendStatus(204))
@@ -29,6 +32,12 @@ app.get('/api/pokemons/:id', (req,res) => {
 
     })
 
-  
+app.post('/api/pokemon', (req,res) => {
+    const id = getUniqueId(pokemons)
+    const pokemoncreated = { ...req.body, ...{id: id, created : new Date() }}
+    pokemons.push(pokemoncreated)
+    const message = `le pokemon ${pokemoncreated.name} à été crée.`
+    res.json(message,pokemoncreated)
+})  
 
 app.listen(port, () => console.log(`notre application node est démarrée sur : http://localhost:${port}`))
